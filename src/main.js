c33a55ebbe4182949e22b646dc572ad5e0be2261
Sarthak-Aswal <sarthakaswalgit@gmail.com>
2024-05-14 23:17:07 +0530

    updates

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
-import '../styles/modern-normalize.css'
-import '../styles/style.css'
-import '../styles/components/header.css'
-import '../styles/components/hero.css'
-import '../styles/components/about.css'
-import '../styles/components/project.css'
-import '../styles/components/contact.css'
-import '../styles/components/footer.css'
-import '../styles/components/mobile-nav.css'
-import '../styles/utils.css'
-import nav from './utils/mobile_nav';
+
+import nav from './utils/mobile_nav.js';
 
 nav();
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        
+        if (entry.isIntersecting) {
+            
+            entry.target.classList.add('show');
+            entry.target.classList.remove('hide');
+            
+        }
+    });
+}, { threshold: 1 });
+
+
+
+
+
+document.querySelectorAll('.hide').forEach(skill => {
+    observer.observe(skill);
+});
+
